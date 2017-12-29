042be1b63585f94a0ae2216760087f2d47929c8c
D10419103 <32352569+D10419103@users.noreply.github.com>
2017-12-29 11:54:13 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -28,8 +28,29 @@ foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];
-            
-           
+            if ($m_message == 1) {
+                                                
+                            $client->replyMessage(array(
+                           'replyToken' => $event['replyToken'],
+                           'messages' => array(
+                             array(
+                                  echo $Q;
+                                 echo " 0 0";
+                                 print $Q1;
+                                 print "1 1 ";
+                                 bot.on('message', function(event) {
+  console.log(event); //把收到訊息的 event 印出來看看
+});
+                                 $bot->sendText($replyToken, "文字訊息");
+                                 $msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("文字訊係");
+$bot->replyMessage($replyToken,$msg);
+                                 
+                                 $msg = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder($packageId,$stickerId);
+$bot->replyMessage($reply_token,$msg);
+                               )
+                            )
+                        	));                          
+            }
         default:
             error_log("Unsupporeted event type: " . $event['type']);
             break;
