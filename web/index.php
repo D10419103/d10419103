b75bc5dd7b4ddd250baa3e89f185ecca2c9b5ede
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-01-11 14:46:43 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -22,7 +22,7 @@ require_once('./LINEBotTiny.php');
 
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
-"https://spreadsheets.google.com/feeds/list/2PACX-1vQM1rw7ng4utfYxkEwZ8qAftWD91VEmdlm0XB6eNvRoO3PkJnCTVA9ABFUzPhR7sfKEZup4R15w0T-M/od6/public/values?alt=json";
+$googledataspi ="https://spreadsheets.google.com/feeds/list/2PACX-1vQM1rw7ng4utfYxkEwZ8qAftWD91VEmdlm0XB6eNvRoO3PkJnCTVA9ABFUzPhR7sfKEZup4R15w0T-M/od6/public/values?alt=json";
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
