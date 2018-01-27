4c316258977ad6fb58f12c99fcbc8641ad09fe7a
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-01-27 17:37:56 +0800

    Update bot.php

--- web/bot.php
+++ web/bot.php
@@ -37,7 +37,8 @@ switch ($event['type']) {
             // 以關鍵字比對文字內容，符合的話將店名/地址寫入
             foreach ($keywords as $keyword) {
                 if (mb_strpos($message['text'], $keyword) !== false) {                      
-                    $store_text = $item['gsx$store_name']['$t'] . " 地址是:" . $item['gsx$store_address']['$t'];                 
+                    $name=$item['gsx$storename']['$t'];
+                    $address=$item['gsx$storeaddress']['$t'];               
               }
             }
         } 
@@ -58,7 +59,7 @@ switch ($event['type']) {
                         ),
                         array(
                             'type' => 'text',
-                            'text' => '介紹你 ' . $store_text . ' 不錯喔',
+                            'text' => '介紹你 ' . $name . ' 不錯喔',
                         )
 
                     ),
