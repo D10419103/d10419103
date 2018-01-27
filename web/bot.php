6a3af29025ee1c67041696bcb4d5cb449f6f6966
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-01-27 16:43:08 +0800

    Update bot.php

--- web/bot.php
+++ web/bot.php
@@ -1,96 +1,94 @@
 <?php
-//DEBUG
-file_put_contents("debug.txt", file_get_contents("php://input"),FILE_APPEND);
-$receive = json_decode(file_get_contents("php://input"));
 
-use LINE\LINEBot;
-use LINE\LINEBot\HTTPClient\GuzzleHTTPClient;
-use LINE\LINEBot\Message\MultipleMessages;
-use LINE\LINEBot\Message\RichMessage\Markup;
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 
-require "vendor/autoload.php";
-require_once "vendor/joshcam/mysqli-database-class/MysqliDb.php";
+require_once('line-bot-sdk-tiny/LINEBotTiny.php');
 
-$setting = require('config.php');
+$channelAccessToken = 'd94WAvqAJBWRXZ3pmnlejuQ7S/Glp8CDK0FHSSLEWlypMdpiPerBs23gk/xsbQjT31RHVd1iq4YVMqqLbYiRRA0AnDPQohV2zFBBwMBK5JchWjB47muK5uiHL2l/JvkepuraSTviQNaPxMjKM7z/jwdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'f09490cd01d030f3bed923ab84c529cd';
 
-$db = new MysqliDb(Array(
-    'host' => $setting['dbhost'],
-    'username' => $setting['dbusername'],
-    'password' => $setting['dbpassword'],
-    'db' => $setting['dbname'],
-    'port' => $setting['dbport'],
-    'charset' => $setting['dbcharset']
-));
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
 
-$db = MysqliDb::getInstance();
+            $json = file_get_contents('https://spreadsheets.google.com/feeds/list/1tQCaj3LUVwH0tBuPrfBY2dOJuF-qzpYEdOqGdNvJRLc/od6/public/values?alt=json');
+            $data = json_decode($json, true);
+            $result = array();
 
-$content = $receive->result[0]->content;
-$text = $content->text;
-$from = $content->from;
-$createdTime = $content->createdTime;
+            foreach ($data['feed']['entry'] as $item) {
+                $keywords = explode(',', $item['gsx$keyword']['$t']);
 
-if (isset($content->location->address)){
-    $address = $content->location->address;
-    $latitude = $content->location->latitude;
-    $longitude = $content->location->longitude;
-}
+                foreach ($keywords as $keyword) {
+                    if (mb_strpos($message['text'], $keyword) !== false) {
+                        $candidate = array(
+                            'thumbnailImageUrl' => $item['gsx$photourl']['$t'],
+                            'title' => $item['gsx$title']['$t'],
+                            'text' => $item['gsx$title']['$t'],
+                            'actions' => array(
+                                array(
+                                    'type' => 'uri',
+                                    'label' => '查看詳情',
+                                    'uri' => $item['gsx$url']['$t'],
+                                    ),
+                                ),
+                            );
+                        array_push($result, $candidate);
+                    }
+                }
+            }
 
-$contentType = $content->contentType;
-
-if (isset($content->contentMetadata->mid)){
-    $sendProfile_mid = $content->contentMetadata->mid;
-    $sendProfile_displayName = $content->contentMetadata->displayName; 
-}
-
-$config = [
-    'channelId' => $setting['channelId'],
-    'channelSecret' => $setting['channelSecret'],
-    'channelMid' => $setting['channelMid']
-];
-
-$bot = new LINEBot($config, new GuzzleHTTPClient($config));
-
-$profile = $bot->getUserProfile([$from]);
-$callbackerDisplayname = $profile['contacts'][0]['displayName'];
-$callbackerPic = $profile['contacts'][0]['pictureUrl'];
-
-$db->insert ('callback_log', array(
-    'from_displayname'=>$callbackerDisplayname,
-    'from_pic'=>$callbackerPic,
-    'from'=>$from,
-    'created_time'=>$createdTime,
-    'content_type'=>$contentType,
-    'address'=>isset($address)?$address:"",
-    'latitude'=>isset($latitude)?$latitude:"",
-    'longitude'=>isset($longitude)?$longitude:"",
-    'sendprofile_mid'=>isset($sendProfile_mid)?$sendProfile_mid:"",
-    'sendprofile_displayname'=>isset($sendProfile_displayName)?$sendProfile_displayName:"",
-    'text'=>isset($text)?$text:""
-));
-
-if (preg_match("/^給個數字/i", $text)) {
-    $param = explode(" ",$text);
-    if ($param[1]!="" && $param[2]!=""){
-	$r = rand($param[1],$param[2]);
-        $bot->sendText([$from], "來，開個{$r}");
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text'].'讓我想想喔…',
+                            ),
+                            array(
+                                'type' => 'template',
+                                'altText' => '為您推薦下列美食：',
+                                'template' => array(
+                                    'type' => 'carousel',
+                                    'columns' => $result,
+                                ),
+                            ),
+                            array(
+                                'type' => 'text',
+                                'text' => '這些都超好吃，真心不騙！',
+                            ),
+                            array(
+                                'type' => 'sticker',
+                                'packageId' => '1',
+                                'stickerId' => '2',
+                            ),
+                        ),
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
     }
-}
-
-if (preg_match("/^你是誰/i", $text)){
-    $markup = (new Markup(1040))
-        ->setAction('event1', '一介資男', 'https://www.mxp.tw/blog/','web')
-        ->addListener('event1', 0, 0, 1040, 1040);
-    $bot->sendRichMessage([$from], 'http://dummyimage.com/1040x1040/000000/fff.jpg', "官方Blog", $markup);
-    $bot->sendText([$from], "敲敲設計有限公司：");
-    $bot->sendLocation([$from],'台北市信義路4段45號',25.0335717,121.5449681);
-}
-
-if (preg_match("/^公車/i", $text)){
-    $param = explode(" ",$text);
-    if ($param[1]!=""){
-        $bot->sendText([$from], "如果你沒亂打公車代號，那現在公車狀態是：\nhttp://pda.5284.com.tw/MQS/businfo2.jsp?routename={$param[1]}");
-    }
-}
-
-$bot->sendText([$from], "您好 {$callbackerDisplayname}，我的使用方式：\n1. 查詢機器人資訊。輸入\n「你是誰」\n2. 指定範圍，隨機選個數字。輸入\n「給個數字 1 100」\n，從1~100隨機選一個數字。\n3. 查詢公車最新動態。輸入\n「公車 藍5」\n，空格後輸入完整公車名（或號碼）");
-
+};
